7dc16c619c99f700e892938b26a59cdc12477830
Tomas Ribinskis <tomas.ribinskis@gmail.com>
2019-08-18 18:01:36 +0300

    New scrapes done

--- bloomberg_scrape.py
+++ bloomberg_scrape.py
@@ -4,16 +4,13 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
-reg_url = "https://www.kilo.lt/kontaktai"
+reg_url = "https://www.bloomberg.com/quote/SPX:IND"
 req = Request(url=reg_url, headers=headers)
 html = urlopen(req).read()
 
-# parse the html using beautiful soup and store in variable `soup`
 soup = BeautifulSoup(html, "html.parser")
 
-# Take out the <div> of name and get its value
-name_box = soup.find("span", attrs={"class": "phone"})
-
+name_box = soup.find("span", attrs={"class": "priceText__1853e8a5"})
 
 # strip() is used to remove starting and trailing
 name = name_box.text.strip()
